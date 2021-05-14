1cf2a1b9297bc73f01aa3bc6dbac2761a76d9c4a
허석 <gjtjr0707@naver.com>
2021-05-15 03:33:35 +0900

    커밋 - 21.05.15 03:33

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -11,7 +11,7 @@ import java.text.NumberFormat
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
-// 주석문 입력
+// 주석문 입력 ㅇㅇㅇ
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
